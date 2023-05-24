711fb5a56fdb341c0f9906546134f1f846c17b01
Myagmarsaikhan B <myagmarsaikhan.b02@gmail.com>
2023-05-24 12:48:56 +0800

    updatems

--- config/db.js
+++ config/db.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  const conn = await mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  const conn = await mongoose.connect(
+    "mongodb+srv://Mygmarsaikhan:MC2002MCc!@cluster0.7ox6oem.mongodb.net/test",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  );
   console.log(`MongoDB холбогдлоо: ${conn.connection.host}`);
 };
 
